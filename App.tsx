91c2a628dd419f0a96eb310433807e0ae4185f69
JoanSeguraG <163119430+JoanSeguraG@users.noreply.github.com>
2025-04-04 11:37:46 +0200

    BooksApp v1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import { useState, useEffect } from 'react';
+import { supabase } from './lib/supabase';
+import Auth from './components/Auth';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { Session } from '@supabase/supabase-js';
 
 export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+  const [newPassword, setNewPassword] = useState('');
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  async function updatePassword() {
+    const { error } = await supabase.auth.updateUser({ password: newPassword });
+
+    if (error) {
+      Alert.alert('Error', error.message);
+    } else {
+      Alert.alert('Contrasenya canviada amb èxit!', 'Pots iniciar sessió amb la nova contrasenya.');
+      setNewPassword('');
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {!session ? (
+        <Auth />
+      ) : (
+        <View>
+          <Text>Usuari: {session.user?.email}</Text>
+          <TextInput
+            placeholder="Nova contrasenya"
+            secureTextEntry
+            value={newPassword}
+            onChangeText={setNewPassword}
+          />
+          <Button title="Canviar Contrasenya" onPress={updatePassword} />
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
